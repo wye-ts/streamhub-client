c757453110c267d328feac667387a311dbf4b713
wye-ts <jacky274865077@gmail.com>
2021-11-14 21:19:39 -0800

    update homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,70 +1,99 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// import CardGroup from 'react-bootstrap/CardGroup';
+// import Card from 'react-bootstrap/Card';
 
 export default function HomePage() {
 	return (
-		<div>
-			<div className="container-fluid">
-				<div style={{ width: '1920px' }} className="row">
-					<div className="col-1"></div>
-					<div className="col-3">
-						<NavLink to="/streaming/gUTtJjV852c">
-							<img
-								className="doraMovie"
-								src="/images/movie1.jpg"
-								alt="Dora"
-							></img>
-							<div className="movie1">Dora Explorer</div>
-							<img
-								className="playButton1"
-								src="/images/PlayButton.png"
-								alt="button1"
-							></img>
-						</NavLink>
+		<div className="home-container">
+			<div className="home-top">
+				<div className="home-red"></div>
+				<NavLink to="/streaming/oqxAJKy0ii4">
+					<div className="image-wrap">
+						<img src="/images/squidgame.jpg" alt="SquidGame"></img>
 					</div>
-					<div className="col-3">
-						<NavLink to="/streaming/8YjFbMbfXaQ">
-							<img
-								className="shangChiMovie"
-								src="/images/shangchi.jpg"
-								alt="ShangChi"
-							></img>
-							<div className="movie2">Shang Chi</div>
-							<img
-								className="playButton2"
-								src="/images/PlayButton.png"
-								alt="button2"
-							></img>
-						</NavLink>
+					{/* <div className="movie2">Squid Game</div> */}
+					{/* <img
+						className="playButton3"
+						src="/images/PlayButton.png"
+						alt="button3"
+					></img> */}
+				</NavLink>
+				<NavLink to="/streaming/oqxAJKy0ii4">
+					<div className="image-wrap">
+						<img src="/images/movie1.jpg" alt="Dora Explorer"></img>
 					</div>
-					<div className="col-3">
-						<NavLink to="/streaming/oqxAJKy0ii4">
-							<img
-								className="squidGame"
-								src="/images/squidgame.jpg"
-								alt="SquidGame"
-							></img>
-							<div className="movie2">Squid Game</div>
-							<img
-								className="playButton3"
-								src="/images/PlayButton.png"
-								alt="button3"
-							></img>
-						</NavLink>
+				</NavLink>
+				<NavLink to="/streaming/8YjFbMbfXaQ">
+					<div className="image-wrap">
+						<img src="/images/shangchi.jpg" alt="Shang Chi"></img>
 					</div>
-					<div className="col-1">
-						<img
-							className="rightClick"
-							src="/images/rightArrow.jpg"
-							alt="right arrow"
-						></img>
-						<img
-							className="leftClick"
-							src="/images/leftArrow.jpg"
-							alt="left arrow"
-						></img>
+				</NavLink>
+				<div className="home-red ">
+					<div>
+						<img src="/images/leftArrow.jpg" alt="left arrow"></img>
+						<img src="/images/leftArrow.jpg" alt="right arrow"></img>
 					</div>
 				</div>
+
+				{/* <div className="col-1"></div>
+				<div className="col-3">
+					<NavLink to="/streaming/gUTtJjV852c">
+						<img
+							className="doraMovie"
+							src="/images/movie1.jpg"
+							alt="Dora"
+						></img>
+						<div className="movie1">Dora Explorer</div>
+						<img
+							className="playButton1"
+							src="/images/PlayButton.png"
+							alt="button1"
+						></img>
+					</NavLink>
+				</div>
+				<div className="col-3">
+					<NavLink to="/streaming/8YjFbMbfXaQ">
+						<img
+							className="shangChiMovie"
+							src="/images/shangchi.jpg"
+							alt="ShangChi"
+						></img>
+						<div className="movie2">Shang Chi</div>
+						<img
+							className="playButton2"
+							src="/images/PlayButton.png"
+							alt="button2"
+						></img>
+					</NavLink>
+				</div>
+				<div className="col-3">
+					<NavLink to="/streaming/oqxAJKy0ii4">
+						<img
+							className="squidGame"
+							src="/images/squidgame.jpg"
+							alt="SquidGame"
+						></img>
+						<div className="movie2">Squid Game</div>
+						<img
+							className="playButton3"
+							src="/images/PlayButton.png"
+							alt="button3"
+						></img>
+					</NavLink>
+				</div>
+				<div className="col-1">
+					<img
+						className="rightClick"
+						src="/images/rightArrow.jpg"
+						alt="right arrow"
+					></img>
+					<img
+						className="leftClick"
+						src="/images/leftArrow.jpg"
+						alt="left arrow"
+					></img>
+				</div> */}
 			</div>
 			<div className="Box">
 				<div className="title">Most Watched Movies</div>
